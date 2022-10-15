63bf3371612e2e680931b59d64b58800df58aacc
CarolinaDume <111905819+CarolinaDume@users.noreply.github.com>
2022-10-14 21:27:27 -0300

    Add files via upload

---
+++ validacionformulario.js
@@ -0,0 +1,74 @@
+
+const formulario = document.getElementById("formulario");
+
+const userName = document.getElementById("userName");
+const userEmail = document.getElementById("userEmail");
+
+const alertSuccess = document.getElementById("alertSuccess");
+const alertName = document.getElementById("alertName");
+const alertEmail = document.getElementById("alertEmail");
+
+const regUserName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+const regUserEmail = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
+
+const mensajeExito = () => {
+    alertSuccess.classList.remove("d-none");
+    alertSuccess.textContent = "Mensaje enviado con éxito";
+};
+
+const mensajeError = (errores) => {
+    errores.forEach((item) => {
+        item.tipo.classList.remove("d-none");
+        item.tipo.textContent = item.msg;
+    });
+};
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    alertSuccess.classList.add("d-none");
+    const errores = [];
+
+    // validar nombre y apellido
+    if (!regUserName.test(userName.value) || !userName.value.trim()) {
+        userName.classList.add("is-invalid");
+
+        errores.push({
+            tipo: alertName,
+            msg: "Formato no válido. Ingresar sólo letras",
+        });
+    } else {
+        userName.classList.remove("is-invalid");
+        userName.classList.add("is-valid");
+        alertName.classList.add("d-none");
+    }
+
+    // validar email
+    if (!regUserEmail.test(userEmail.value) || !userEmail.value.trim()) {
+        userEmail.classList.add("is-invalid");
+
+        errores.push({
+            tipo: alertEmail,
+            msg: "Escribir un correo válido",
+        });
+    } else {
+        userEmail.classList.remove("is-invalid");
+        userEmail.classList.add("is-valid");
+        alertEmail.classList.add("d-none");
+    }
+
+    if (errores.length !== 0) {
+        mensajeError(errores);
+        return;
+    }
+
+    console.log("Formulario enviado con éxito.");
+    mensajeExito();
+});
+  //Resetea el formulario luego de enviado.
+
+ //ver reseteo se resetea si vamos escribiendo ya un campo mal borra todo
+ 
+  
+
+   
