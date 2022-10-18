fb59fe274e2cf913754c64e9c8706757fd481bd8
Nuria <53791646+Nuria1982@users.noreply.github.com>
2022-10-18 15:27:16 -0300

    Add files via upload

--- java/encabezado.js
+++ java/encabezado.js
@@ -1,9 +1,14 @@
 encabezado=`
+<div class="container" id="app">
+                <p>{{clima.name}}</p>
+                <p>{{clima.coord.lon}}</p>
+                <p>{{clima.main.humidity}}</p>
+    </div>
 <h1 class="titulo">Agromet</h1>
 <h2 class="subtitulo">Balcarce</h2>
-<nav>
-    <a class="link" href="nosotros.html">Nosotros</a>
-    <a class="link" href=".contactos">Contacto</a>
+<nav id="nav">
+    <a class="link" href="#nosotros.html">Nosotros</a>
+    <a class="link" href="#contactos">Contacto</a>
 </nav>
 `
 document.write(encabezado)
--- java/footer.js
+++ java/footer.js
@@ -1,10 +1,10 @@
 footer=`
-<div class="contactos">
+<div id="contactos">
                 <h5>Podes escribirnos en <a href="mailto:agromet@agromet.gob.ar">agromet@agromet.com.ar</a></h5>
                 <h5>o buscanos en las redes sociales</h5>
-                    <img src="./instagram.png" alt="instagram" style="width:24px;height:24px;">
-                    <img src="./whatsapp.png" alt="wsp" style="width:24px;height:24px;">
-                    <img src="./twitter.png" alt="twitter" style="width:24px;height:24px;">
+                    <img src="./Imagenes/instagram.png" alt="instagram" style="width:24px;height:24px;">
+                    <img src="./Imagenes/whatsapp.png" alt="wsp" style="width:24px;height:24px;">
+                    <img src="./Imagenes/twitter.png" alt="twitter" style="width:24px;height:24px;">
                 <br>
                 <a class="link" href="#nav">Volver</a>           
         </div>
